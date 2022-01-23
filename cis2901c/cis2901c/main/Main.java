cd98edc280ae6c220a7606400ed3dfe7ba0efebb
M. Corey Glass <dhexcxr@gmail.com>
2022-01-23 16:56:43 -0500

    doc: start comment project

--- cis2901c/cis2901c/main/Main.java
+++ cis2901c/cis2901c/main/Main.java
@@ -1,7 +1,6 @@
 package cis2901c.main;
 
 import java.sql.SQLException;
-//import java.sql.Statement;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
@@ -12,23 +11,23 @@ import org.eclipse.swt.widgets.Shell;
 import cis2901c.listeners.DbServices;
 
 public class Main {
-//	private static Gui gui = null;
 	private static Shell shell = null;
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		
+
+		// Start GUI
 		Display display = new Display();
-//		Shell shell = new Shell(display);
 		shell = new Shell(display);
 		shell.setLayout(new GridLayout(1, false));
 		shell.setText("Service Salamander");
 		@SuppressWarnings("unused")
 		Gui gui = new Gui(shell, SWT.NONE);
-//		gui = new Gui(shell, SWT.NONE);
 		shell.pack();
 		shell.open();
+		
+		// Connect to SQL Database
 		DbServices.connectToDb();
 		if (DbServices.isConnected()) {
 			while (!shell.isDisposed()) {
@@ -36,19 +35,20 @@ public class Main {
 					display.sleep();
 			}
 		} else {
-			// error dialog box here
-			System.out.println("error dialog box here");
+			System.out.println("DB Connection error dialog box");
 			MessageBox dbConnectionError = new MessageBox(shell, SWT.ERROR);
 			dbConnectionError.setText("DB Error");
 			dbConnectionError.setMessage("Unable to connect to database");
 			dbConnectionError.open();
 		}
+		
+		// disconnect from SQL DB before close
 		DbServices.disconnectFromDb();
 		display.dispose();
 	}
 	
 	public static Shell getShell() {
-//		gui = null;
+		// TODO see if we can provide shell some way other than a Main class getter (follow where this method is called)
 		if (shell.equals(null)) {
 			System.out.println("Error loading Gui: shell cannot be null");
 			System.out.println(new Throwable().getStackTrace());
@@ -56,41 +56,4 @@ public class Main {
 		}
 		return shell;
 	}
-	
-//	protected static Gui getGui() {
-//		gui = null;
-//		if (gui.equals(null)) {
-//			System.out.println("Error loading Gui: Gui cannot be null");
-//			System.out.println(new Throwable().getStackTrace());
-//			System.exit(-1);
-//		}
-//		return gui;
-//	}
-		
-	// TODO research if creating a new connection everytime we need one hurts performance
-	// see if I want to setAutoCommit to false before returning Connection, every call would have to connection.commit, or .rollback if an exception is caught
-//	protected static Connection getDbConnection() {
-////		return mainDbConnection;
-//		String url = "jdbc:mysql://localhost:3306/cis2901c";
-//		String user = "TestUser";
-//		String pass = "test";
-//		Connection dbConnection = null;
-//		try {
-//			dbConnection = DriverManager.getConnection(url, user, pass);
-//		} catch (SQLException e) {
-//			System.out.println("Error connecting to database");
-//			e.printStackTrace();
-//		}
-//		return dbConnection;
-//	}
-	
-//	protected static Connection getDbConnection() {
-//		return DbServices.getDbConnection();
-//	}
-	
-//	public static Statement getDbStatement() {
-//		
-//		return mainDbConnection.createStatement();
-//	}
-
 }
