ccfafda8577d0b52c8bbcdc315278982a71b70f9
M. Corey Glass <dhexcxr@gmail.com>
2022-02-08 17:30:43 -0500

    fix: change abstract methods in MyTable to public

--- cis2901c/cis2901c/objects/MyTable.java
+++ cis2901c/cis2901c/objects/MyTable.java
@@ -3,28 +3,20 @@ package cis2901c.objects;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 
-public class MyTable extends Table {
+public abstract class MyTable extends Table {
 
-	public MyTable(Composite parent, int style) {
+	protected MyTable(Composite parent, int style) {
 		super(parent, style);
 	}
 	
-	public void paint(Object object) {
-		System.out.println("This shouldn't ever be called");
-		System.out.println(Thread.currentThread().getStackTrace());
-	}
+	public abstract void paint(Object object);
 	
-	public void paint(Object[] objects) {
-		System.out.println("This shouldn't ever be called");
-		System.out.println(Thread.currentThread().getStackTrace());
-	}
+	public abstract void paint(Object[] objects);
 	
 	@Override
 	protected void checkSubclass() {
 		// Disable the check that prevents subclassing of SWT components
 	}
 
-	public void sort(int i) {
-		// TODO Auto-generated method stub
-	}
+	public abstract void sort(int i);
 }
