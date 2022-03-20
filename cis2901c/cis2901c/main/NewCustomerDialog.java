2400bab4cd493a639c8f8c965c884a85df993b5d
M. Corey Glass <dhexcxr@gmail.com>
2022-03-20 10:21:53 -0400

    fix: set Last Name text box to white when opening an Existing Customer
    do not highlight Last Name text box in red, indicating that a Last Name
    is still required, when opening a Customer to edit. If we're opening an
    existing Customer there will already be a Last Name, therefore we should
    not alert the user that it is missing

--- cis2901c/cis2901c/main/NewCustomerDialog.java
+++ cis2901c/cis2901c/main/NewCustomerDialog.java
@@ -222,6 +222,7 @@ public class NewCustomerDialog extends Dialog {
 	private void setupListeners() {
 		txtFirstName.addModifyListener(new InfoTextBoxModifyListener(txtFirstName));
 		txtLastName.addModifyListener(new RequiredTextBoxModifyListener(txtLastName));
+		txtLastName.setBackground(SWTResourceManager.getColor(255, 255, 255));		// WHITE
 		txtAddress.addModifyListener(new InfoTextBoxModifyListener(txtAddress));
 		txtCity.addModifyListener(new InfoTextBoxModifyListener(txtCity));
 		txtState.addModifyListener(new InfoTextBoxModifyListener(txtState));
