7c9b8b2e9dbb1440a5fef17892c8ae6378908647
M. Corey Glass <dhexcxr@gmail.com>
2022-03-24 18:32:12 -0400

    feat: merge branch InvoicePart-JobPart into branch master
    done refactoring Invoices from using the *TableItem as the object to
    save into the DB to using InvoicePart and JobPart as objects to save
    into DB. Also fix bugs, add stability, minor features.
    
    This was started with commit bbc11a2 on 2022.03.22 at 20:30:50

--- cis2901c/cis2901c/main/Main.java
+++ cis2901c/cis2901c/main/Main.java
@@ -25,7 +25,6 @@ public class Main {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
